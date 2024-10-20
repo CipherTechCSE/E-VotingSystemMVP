1a5a79aee882b704b47df13d7c20b69e1a505f0c
Charindu Thisara <thisaraimc@gmail.com>
2024-10-21 00:42:52 +0530

    Add sample structure

--- vote_authority_service/src/main/java/org/ciphertech/vote_authority_service/VoteAuthorityServiceApplication.java
+++ vote_authority_service/src/main/java/org/ciphertech/vote_authority_service/VoteAuthorityServiceApplication.java
@@ -3,7 +3,9 @@ package org.ciphertech.vote_authority_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = "org.ciphertech"
+)
 public class VoteAuthorityServiceApplication {
 
     public static void main(String[] args) {
