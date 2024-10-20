9ff44212b4bf7dbe729d14293d3ebb8715b6e607
Charindu Thisara <thisaraimc@gmail.com>
2024-10-20 19:00:41 +0530

    Fix spring issues

---
+++ vote_authority_service/src/main/java/org/ciphertech/vote_authority_service/VoteAuthorityServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ciphertech.vote_authority_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteAuthorityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteAuthorityServiceApplication.class, args);
+    }
+
+}
